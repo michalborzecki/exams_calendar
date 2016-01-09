5537ac23a27f8f82cb7807b9a21f9146ac9a4d08
Jatkowskee <jtk.piotr@gmail.com>
2016-01-09 18:08:43 +0100

    AuthAction new ActionBuilder

--- app/controllers/ExamController.scala
+++ app/controllers/ExamController.scala
@@ -36,80 +36,61 @@ class ExamController @Inject() (repo: ExamRepository, oauth: Authentication, val
     )(EditExamForm.apply)(EditExamForm.unapply)
   }
 
-  def index = Action { request =>
-    if (oauth isAuthenticated request) {
-      Ok(views.html.index(addExamForm))
-    }
-    else {
-      Redirect(routes.Authentication.authenticate())
-    }
+  def index = AuthAction { implicit request =>
+    Ok(views.html.index(addExamForm))
   }
 
-  def addExam = Action.async { implicit request =>
-    if(oauth isAuthenticated request) {
-      addExamForm.bindFromRequest.fold(
-        errorForm => {
-          Future.successful(Ok(views.html.index(errorForm)))
-        },
-        exam => {
-          repo.create(exam.name, exam.level, exam.date).map { _ =>
-            Redirect(routes.ExamController.index)
-          }
-        }
-      )
-    }
-    else Future { Redirect(routes.Authentication.authenticate()) }
-  }
-
-  def getExams = Action.async { implicit request =>
-    if(oauth isAuthenticated request) {
-      repo.list().map { exams =>
-        Ok(Json.toJson(exams))
-      }
-    }
-    else Future { Redirect(routes.Authentication.authenticate()) }
-  }
-
-  def editExam(id: Long) = Action.async { implicit request =>
-    if(oauth isAuthenticated request) {
-      repo.getById(id).map(exams =>
-        if (exams.nonEmpty)
-          Ok(views.html.editExam(editExamForm.bind(Map(
-            "id" -> exams.head.id.toString,
-            "name" -> exams.head.name,
-            "level" -> exams.head.level.toString,
-            "date" -> exams.head.date.toString))))
-        else
+  def addExam = AuthAction.async { implicit request =>
+    addExamForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(Ok(views.html.index(errorForm)))
+      },
+      exam => {
+        repo.create(exam.name, exam.level, exam.date).map { _ =>
           Redirect(routes.ExamController.index)
-      )
+        }
+      }
+    )
+  }
+
+  def getExams = AuthAction.async { implicit request =>
+    repo.list().map { exams =>
+      Ok(Json.toJson(exams))
     }
-    else Future { Redirect(routes.Authentication.authenticate()) }
+  }
+
+  def editExam(id: Long) = AuthAction.async { implicit request =>
+    repo.getById(id).map(exams =>
+      if (exams.nonEmpty)
+        Ok(views.html.editExam(editExamForm.bind(Map(
+          "id" -> exams.head.id.toString,
+          "name" -> exams.head.name,
+          "level" -> exams.head.level.toString,
+          "date" -> exams.head.date.toString))))
+      else
+        Redirect(routes.ExamController.index)
+    )
   }
 
   def saveExam = Action.async { implicit request =>
-    if(oauth isAuthenticated request) {
-      editExamForm.bindFromRequest.fold(
-        errorForm => {
-          Future.successful(Ok(views.html.editExam(errorForm)))
-        },
-        exam => {
-          repo.save(exam.id, exam.name, exam.level, exam.date).map { _ =>
-            Redirect(routes.ExamController.index)
-          }
+    editExamForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(Ok(views.html.editExam(errorForm)))
+      },
+      exam => {
+        repo.save(exam.id, exam.name, exam.level, exam.date).map { _ =>
+          Redirect(routes.ExamController.index)
         }
-      )
-    }
-    else Future { Redirect(routes.Authentication.authenticate()) }
+      }
+    )
   }
 
   def deleteExam(id: Long) = Action.async { implicit request =>
-    if(oauth isAuthenticated request) {
-      repo.delete(id).map(exams =>
-        Redirect(routes.ExamController.index)
-      )
-    }
-    else Future { Redirect(routes.Authentication.authenticate()) }
+    repo.delete(id).map(exams =>
+      Redirect(routes.ExamController.index)
+    )
   }
+  
 }
 
 case class CreateExamForm(name: String, level: Int, date: Date)
